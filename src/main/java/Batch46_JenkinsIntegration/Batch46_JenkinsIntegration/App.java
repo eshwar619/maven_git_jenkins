f8716f0cf8e17151c753b3c43476caf4059c2c2c
eshwar619 <raoeshwar089@gmail.com>
2024-06-26 10:57:58 +0530

    my project maven git

---
+++ src/main/java/Batch46_JenkinsIntegration/Batch46_JenkinsIntegration/App.java
@@ -0,0 +1,13 @@
+package Batch46_JenkinsIntegration.Batch46_JenkinsIntegration;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/countries/country.java
@@ -0,0 +1,63 @@
+package countries;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class country {
+  @Test
+  public void ind()
+  {
+	  String name ="india";
+	  Assert.assertEquals(name, "india");
+	  System.out.println("the countey name is "+name);
+	  
+  }
+  @Test
+  public void ban()
+  {
+	  String name ="bangladesh";
+	  Assert.assertEquals(name, "bangladesh");
+	  System.out.println("the countey name is "+name);
+	  
+  }
+  @Test
+  public void sl()
+  {
+	  String name ="srilanka";
+	  Assert.assertEquals(name, "srilanka");
+	  System.out.println("the countey name is "+name);
+	  
+  }
+  @Test
+  public void aus()
+  {
+	  String name ="austrila";
+	  Assert.assertEquals(name, "austrila");
+	  System.out.println("the countey name is "+name);
+	  
+  }
+  @Test
+  public void wi()
+  {
+	  String name ="westindies";
+	  Assert.assertEquals(name, "westindies");
+	  System.out.println("the countey name is "+name);
+	  
+  }
+  @Test
+  public void pak()
+  {
+	  String name ="pakistan";
+	  Assert.assertEquals(name, "pakistan");
+	  System.out.println("the countey name is "+name);
+	  
+  }
+  @Test
+  public void nz()
+  {
+	  String name ="newzland";
+	  Assert.assertEquals(name, "newzland");
+	  System.out.println("the countey name is "+name);
+	  
+  }
+}
